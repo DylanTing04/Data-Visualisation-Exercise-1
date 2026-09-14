322e403ffdfbc12d9ea9425c32eda9d71f4dac36
Joch <dynamic.co.collaboration@gmail.com>
2026-09-14 23:16:09 +0800

    Implement dynamic navbar on page load
    Replaced console log with DOMContentLoaded event listener to dynamically generate the navigation bar based on the current page.

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// Simple script: confirms the webpage has loaded in the browser console
-console.log("Website loaded successfully!");
+document.addEventListencer("DOMContentLoaded",() => {
+  const navContainer = document.GetElementByID("navbar-pl");
+  if (!navContainer) return;
+
+  const currentPage = window.location.pathname.split("/").pop() || "index.html";
+  navContainer.innerHTML = `<header>
+  <div class = "navbar">
+    <a href="index.html">
+      <img src="PowerIcon.png" alt="Power Logo" class="power-logo" /> 
+    </a>
+    <nav class="nav=menu">
+      <a href="index.html" class = "nav-link ${currentPage === "index.html" ? "active" : ""}">Index</a>
+      <a href="home.html" class = "nav-link ${currentPage === "home.html" ? "active" : ""}">Home</a>
+      <a href="television.html" class = "nav-link ${currentPage === "televisions.html" ? "active" : ""}">Television</a>
+      <a href="about.html" class = "nav-link ${currentPage === "about.html" ? "active" : ""}">About Us</a>
+    </nav>
+  </header>`;
+});
